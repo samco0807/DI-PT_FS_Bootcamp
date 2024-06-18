1233cd43372dc334f7a5fd6af8327c44175e521c
Samuel Cohen <samco0807@gmail.com>
2024-06-18 20:21:27 +0300

    save

--- week10-asyncJS/MPD/DC-currency.js
+++ week10-asyncJS/MPD/DC-currency.js
@@ -1,79 +1,70 @@
 // create a function to fetch currency conversion rate
 
+// Fetch the currency in the API
 const fetchCurrencies = async () => {
-const apiUrl = `https://v6.exchangerate-api.com/v6/2b9799ae7f2f243b7fa1e9da/codes`;
-  
-const response = await fetch(apiUrl);
-  if (response.ok) {
+  try {
+    const response = await fetch(
+      `https://v6.exchangerate-api.com/v6/2b9799ae7f2f243b7fa1e9da/codes`
+    );
     const data = await response.json();
-    console.log(data);
-    const rate = data.supported_codes
-    console.log(rate);
-    return data.result;
-  } else {
-    throw new Error("Failed to retrieve currency data");
+    populateDropDown(data.supported_codes);
+  } catch (error) {
+    console.log("Failed to fetch currency: ", error);
   }
 };
-fetchCurrencies()
 
-
+// ADD ENTRIES IN THE DROPDOWN MENU
 const populateDropDown = (codes) => {
-    // console.log("codes: ", codes);
-  const entries = Object.entries(codes);
-//   console.log("entries", entries);
-
-  for (const entry of entries) {
-    const [index, [code, name]] = entry;
+  // select the from element
+  const fromDropDown = document.querySelector("#from");
+  // select the "to" element
+  const toDropDown = document.querySelector("#to");
+  // For each code in the Object
+  codes.forEach(([code, name]) => {
     const option = document.createElement("option");
-    const option2 = document.createElement("option2");
-    option.innerText = name;
-    option2.innerText = name;
     option.value = code;
+    option.text = `${name} (${code})`;
+    fromDropDown.appendChild(option);
+
+    const option2 = document.createElement("option");
     option2.value = code;
-    document.getElementById("from").appendChild(option);
-    document.getElementById("to").appendChild(option2);
-    
-  }
+    option2.text = `${name} (${code})`;
+    toDropDown.appendChild(option2);
+  });
 };
 
-populateDropDown()
+// const button = document.querySelector("#convert");
+// button.addEventListener("click", fetchCurrencies);
 
-//   function to convert the currency amount
-const convertCurrency = async () => {
+const handleClick = async () => {
   const fromCurrency = document.querySelector("#from").value;
   const toCurrency = document.querySelector("#to").value;
-  const amount = parseFloat(document.getElementById("amount").value);
+  const amount = document.querySelector("#changeCurrency").value;
 
-  const conversionRates = await retrieveCurrency();
-  const initialRate = conversionRates[initialCurrency];
-
-  try {
-    const response = await fetch(apiUrl);
-    if (response.ok) {
-      const data = await response.json();
-      return data.result;
+  if (fromCurrency && toCurrency && amount) {
+    const url = `https://v6.exchangerate-api.com/v6/2b9799ae7f2f243b7fa1e9da/pair/${fromCurrency}/${toCurrency}/${amount}`;
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      const result = data.conversion_result;
+      document.querySelector(
+        "#result"
+      ).innerText = `${amount} ${fromCurrency} = ${result} ${toCurrency}`;
+    } catch (error) {
+      console.log("Failed to convert currency", error);
     }
-    throw new Error("The currency does not exist");
-  } catch (error) {
-    console.log("Serveur error", error);
+  } else {
+    document.querySelector("#result").innerText = "Please fill all the fields";
   }
 };
 
-const button = document.getElementById("convert");
-button.addEventListener("click", fetchCurrencies);
+// Display the conversion automatic while typing on the keyboard
+document
+  .querySelector("#changeCurrency")
+  .addEventListener("keyup", handleClick);
 
-//   function to display the currency change in the DOM
-// const displayCurrency=async()=>{
-//         // put the currency in a variable
-//       const currency=await convertCurrency()
-//     //   select the infos section to display the informations
-//       const currencyAmount=document.querySelector(".result")
-//     //   insert the converted currency in the HTML
-//       currencyAmount.innerHTML=`
-//       <h4>currency: ${currency.currencyAmount}</h4>
-//       `
-//   }
+// Change automatically the amount according to currency
+document.querySelector("#from").addEventListener("change", handleClick);
+document.querySelector("#to").addEventListener("change", handleClick);
 
-// Select the button and convert the currency
-// const button = document.querySelector("#changeCurrency");
-//           button.addEventListener("click", displayCurrency);
+fetchCurrencies();
