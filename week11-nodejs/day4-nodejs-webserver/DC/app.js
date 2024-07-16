393136792c0fd255dc1d4d78b93069abf2bc14be
Samuel Cohen <samco0807@gmail.com>
2024-07-16 19:34:44 +0300

    save

--- week11-nodejs/day4-nodejs-webserver/DC/app.js
+++ week11-nodejs/day4-nodejs-webserver/DC/app.js
@@ -1,17 +1,46 @@
 import axios from "axios";
-import emojis from "emojis";
+import { emojis } from "emojis.js";
 
-// Select form and add an even to the form
-document.querySelector("#emojiGameForm").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  // select the value of the select a attribute
-  const userSelectedEmoji = document.querySelector("#userSelectedEmoji").value;
+// Create function to retrieve random emoji
+const getRandomEmoji = async () => {
   try {
-    const response = await axios("/submit-guess", { userSelectedEmoji });
-    const result = response.data;
-    document.querySelector("#score").innerHTML = `
-    <h2>result: ${result}, Score:${result+score}</h2>`;
+    const response = await axios.get("/emoji");
+    return response.data.emojis;
   } catch (error) {
-    console.log("Error submitting guess: ", error);
+    console.log("Error fetching random emoji", error);
   }
-});
+};
+
+// Function to check if the user selected emoji name matches the random emoji
+document
+  .querySelector("#emojiGameForm")
+  .addEventListener("submit", async (e) => {
+    e.preventDefault();
+    // select the value of the select a attribute
+    const userSelectedEmojiName = document.querySelector(
+      "#userSelectedEmojiName"
+    ).value;
+    try {
+      let score = 0;
+      let finalScore = 0;
+      const response = await axios.post("/submitguess", {
+        userSelectedEmojiName,
+      });
+      const result = response.data;
+      // While the name selected by the user matches the emojis, continue
+      do {
+        document.querySelector("#score").innerHTML = `
+      <h2>Great Job</h2>, 
+      <h2>result: ${score}</h2>, 
+      `;
+        score++;
+      } while (userSelectedEmojiName == getRandomEmoji());
+
+      finalScore = score;
+    } catch (error) {
+      console.log("Error submitting guess: ", error);
+    }
+  });
+
+const submitEmoji = document.querySelector("#submitEmojiButton");
+submitEmoji.addEventListener("click", handleClick);
--- week11-nodejs/day4-nodejs-webserver/DC/server.js
+++ week11-nodejs/day4-nodejs-webserver/DC/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { emojis } from "./emojis";
+import {emojis} from "./emojis";
 
 const app = express();
 app.use(express.json());
@@ -11,14 +11,13 @@ app.get("/emoji", async (req, res) => {
     // Generate a random emoji
     const randomIndex = Math.floor(Math.random() * emojis.length);
     const randomEmoji = emojis[randomIndex];
-
     res.json({ emoji: randomEmoji });
   } catch (error) {
     console.log("Error fetching posts: ", error);
     res.status(500).json({ error: "Internal server error" });
   }
 
-  app.post("/submit-guess", (req, res) => {
+  app.post("/submitguess", (req, res) => {
     try {
       const { userSelectedEmoji } = req.body;
       // select a random Emoji
@@ -27,12 +26,12 @@ app.get("/emoji", async (req, res) => {
       const randomEmoji = emojis[randomIndex];
       let result;
       let score = 0;
-      // if the select user emoji is egal to the random emoji name
-      if (userSelectedEmoji == randomEmoji.name) {
+      // if the selected name user emoji is egal to the random emoji
+      if (selectedEmojiName == randomEmoji.name) {
         result = "Great Job";
         score = +1;
       } else {
-        result = "No you guess wrong";
+        result = "No you guessed wrong";
       }
     } catch (error) {
       console.log("Error handling guess: ", error);
