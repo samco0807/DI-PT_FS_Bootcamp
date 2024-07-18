084a8df3e684a0f7a59f786d9516b04e6dfe9b2a
Samuel Cohen <samco0807@gmail.com>
2024-07-18 15:47:52 +0300

    save

--- week9-advJS_asyncJS/day5-MPD/index.js
+++ week9-advJS_asyncJS/day5-MPD/index.js
@@ -1,36 +1,38 @@
 import { robots } from "./assets.js";
+console.log("Hello");
 
 // Select the elements in the HTML
 const cards = document.querySelector(".cards");
-const searchBar = document.querySelectorAll("#searchbar");
+
 
 // function to display the robots in the cards
-const displayRobots = (robotToDisplay) => {
-  robotToDisplay.forEach((robot) => {
+const displayRobots = () => {
+  robots.forEach((robot) => {
     const robotCard = document.createElement("div");
     robotCard.className = "robot-card";
     robotCard.innerHTML = `
-<div class="card" style="width: 18rem;">
+<div class="card" style="width: 18rem; ">
   <img src="${robot.image}" class="card-img-top" alt="">
   <div class="card-body">
     <h5 class="card-title">${robot.name}</h5>
     <p class="card-text">${robot.email}</p>
   </div>
-</div>;
+</div>
     `;
     cards.appendChild(robotCard);
   });
 };
 
 // function to filter a specific robot according to the name
-const searchRobot = async () => {
-  searchBar.addEventListener("keyup", () => {
-    const searchTerm = searchBar.value.tolowerCase();
+const searchRobot = () => {
+    const searchTerm = searchBar.value.toLowerCase();
     const filterRobots = robots.filter((robot) =>
-      robot.name.tolowerCase().includes(searchTerm)
+      robot.name.toLowerCase().includes(searchTerm())
     );
-    displayRobots(filterRobots);
-  });
-};
+  };
+;
 
-searchRobot();
+const searchBar = document.querySelectorAll("#searchbar");
+searchBar.addEventListener("keyup", (e)=> searchRobot)
+
+displayRobots();
